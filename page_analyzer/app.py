61dbe8e93a40582f212c819ea59d897e55439bba
Elena Dobruk <elena.dobruk@yandex.ru>
2024-06-04 16:50:11 +0300

    added basic interface in index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -8,5 +8,5 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def starting_page():
+    return render_template('index.html')
