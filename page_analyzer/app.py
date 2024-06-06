14521f57c5621ba83cf8a72daf96487f51ec78a1
Elena Dobruk <elena.dobruk@yandex.ru>
2024-06-06 16:36:05 +0300

    edit app.py with first routes, edit index.html, created html files for future pages

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import psycopg2
 import os
+import datetime
+import requests
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-@app.route("/")
-def starting_page():
-    return render_template('index.html')
+@app.route('/')
+def index():
+    messages = get_flashed_messages(with_categories=True)
+    return render_template(
+        'index.html',
+        messages=messages,
+    )
+
+
+@app.route('/urls')
+def get_urls():
+    urls = future_get_database()
+    messages = get_flashed_messages(with_categories=True)
+    return render_template(
+        'show.html',
+        urls=urls,
+        messages=messages,
+    )
