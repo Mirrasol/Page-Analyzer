c40be88ee4a9b2ddfa6d74fdfb13c751d9990b43
Elena Dobruk <elena.dobruk@yandex.ru>
2024-06-10 16:00:03 +0300

    add url-checks table to database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS urls_checks;
 
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
+    created_at DATE
+);
+
+CREATE TABLE urls_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint,
+    status_code bigint,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
     created_at DATE
 );
